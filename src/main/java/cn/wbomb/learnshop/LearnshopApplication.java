b67199f4eee49c7da0e999fa9c6bc08dab449310
georgeT <jobsteng@163.com>
2021-04-21 10:23:23 +0800

    first commit

---
+++ src/main/java/cn/wbomb/learnshop/LearnshopApplication.java
@@ -0,0 +1,13 @@
+package cn.wbomb.learnshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnshopApplication.class, args);
+	}
+
+}
